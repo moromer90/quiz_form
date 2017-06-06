02e8b952d79b6e6ae31b247df9f447810d9f7d20
Alex <aliaksei@ikasle.aeg.es>
2017-06-06 20:04:10 +0200

    Found a bug that duplicates results if retry to choose smth for second, third or n-th time. Also we could change multiple IFs in main.js with (i=first_id_in_array; i<=last_id_in_array; i++) and cycle to paint a result.... But it's ok... I understand that's my time is up...)

--- js/main.js
+++ js/main.js
@@ -1,41 +1,81 @@
-$(document).ready(function(){
-var group1=""; var group2=""; var group3="";var result="";
+$(document).ready(function () {
+    var group1 = "";
+    var group2 = "";
+    var group3 = "";
+    var result = "";
 
-function check_form(){
-    if (!group1 || !group2 || !group3) {
-    console.log("empty fields in one of 3 groups");
-}    else {publish_results()}
-}
-    
-$('label.group1 img').click(function(){
-group1=(this.id);//console.log(group1);
-check_form();
-}) 
-$('label.group2 img').click(function(){
-group2=(this.id);//console.log(group2);
-check_form();
-})
-$('label.group3 img').click(function(){
-group3=(this.id);//console.log(group3);
-check_form();
-})
+    function check_form() {
+        if (!group1 || !group2 || !group3) {
+            console.log("empty fields in one of 3 groups");
+        }
+        else {
+            $('.message').empty();
+            publish_results()
+        }
+    }
+    $('label.group1 img').click(function () {
+        group1 = (this.id); //console.log(group1);
+        check_form();
+    })
+    $('label.group2 img').click(function () {
+        group2 = (this.id); //console.log(group2);
+        check_form();
+    })
+    $('label.group3 img').click(function () {
+        group3 = (this.id); //console.log(group3);
+        check_form();
+    })
 
-function publish_results(){
-    if      (group1==1) {result=result+('<p>Your choice is cryptocoin 1</p>');console.log(result);}
-    else                {result=result+('<p>Your choice is cryptocoin 2</p>');console.log(result);}
-    
-    if      (group2==3) {result=result+('<p>Your choice is cryptocoin 3</p>');console.log(result);}
-    else if (group2==4) {result=result+('<p>Your choice is cryptocoin 4</p>');console.log(result);}
-    else if (group2==5) {result=result+('<p>Your choice is cryptocoin 5</p>');console.log(result);}
-    else if (group2==6) {result=result+('<p>Your choice is cryptocoin 6</p>');console.log(result);}
-    else if (group2==7) {result=result+('<p>Your choice is cryptocoin 7</p>');console.log(result);}
-    else if (group2==8) {result=result+('<p>Your choice is cryptocoin 8</p>');console.log(result);}
-    else if (group2==9) {result=result+('<p>Your choice is cryptocoin 9</p>');console.log(result);}
-    else                {result=result+('<p>Your choice is cryptocoin 10</p>');console.log(result);}
-
-    if      (group3==11) {result=result+('<p>Your choice is cryptocoin 11</p>');console.log(result);}
-    else                 {result=result+('<p>Your choice is cryptocoin 12</p>');console.log(result);}
-
-    $('.message').append(result+"<h4>You're definitely cryptoaddicted. Try to talk about it with your doctor.");
-}
+    function publish_results() {
+        if (group1 == 1) {
+            result = result + ('<p>Your choice is cryptocoin 1</p>');
+            console.log(result);
+        }
+        else {
+            result = result + ('<p>Your choice is cryptocoin 2</p>');
+            console.log(result);
+        }
+        if (group2 == 3) {
+            result = result + ('<p>Your choice is cryptocoin 3</p>');
+            console.log(result);
+        }
+        else if (group2 == 4) {
+            result = result + ('<p>Your choice is cryptocoin 4</p>');
+            console.log(result);
+        }
+        else if (group2 == 5) {
+            result = result + ('<p>Your choice is cryptocoin 5</p>');
+            console.log(result);
+        }
+        else if (group2 == 6) {
+            result = result + ('<p>Your choice is cryptocoin 6</p>');
+            console.log(result);
+        }
+        else if (group2 == 7) {
+            result = result + ('<p>Your choice is cryptocoin 7</p>');
+            console.log(result);
+        }
+        else if (group2 == 8) {
+            result = result + ('<p>Your choice is cryptocoin 8</p>');
+            console.log(result);
+        }
+        else if (group2 == 9) {
+            result = result + ('<p>Your choice is cryptocoin 9</p>');
+            console.log(result);
+        }
+        else {
+            result = result + ('<p>Your choice is cryptocoin 10</p>');
+            console.log(result);
+        }
+        if (group3 == 11) {
+            result = result + ('<p>Your choice is cryptocoin 11</p>');
+            console.log(result);
+        }
+        else {
+            result = result + ('<p>Your choice is cryptocoin 12</p>');
+            console.log(result);
+        }
+        $('.message').append(result + "<h4>You're definitely cryptoaddicted. Try to talk about it with your doctor.");
+        result = "";
+    }
 });
